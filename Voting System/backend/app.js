29db802cdef522c754dd22f9dca3171e2f359465
Freny Chauhan <chauhanfreny01@gmail.com>
2025-10-21 21:33:11 +0530

    Initial backend setup and connected db to neon

---
+++ Voting System/backend/app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg"); 
+const config = require("config");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const cors = require("cors");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+const http = require("http");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+
+const allowedOrigins = ["http://localhost:5173"];
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET || "your-super-secret-key",
+  })
+);
+app.use(flash());
+app.use(express.static(path.join(__dirname, "../public")));
+
+// const pool=new Pool({
+//     host:process.env.PGHOST,
+//     database:process.env.PGDATABASE,
+//     user:process.env.PGUSER,
+//     password:process.env.PGPASSWORD,
+//     port:5432,
+//     ssl:{
+//         require:true,
+//         rejectUnauthorized: false,  
+//     }
+// })
+
+// pool.connect()
+//   .then(() => console.log("✅ Connected to Neon PostgreSQL!"))
+//   .catch(err => console.error("❌ Database connection error:", err.stack));
+
+
+app.get("/", (req, res) => {
+  res.send("Server running...");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
